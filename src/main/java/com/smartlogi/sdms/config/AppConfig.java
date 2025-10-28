c682579f4c8691192c338b833ebf043fde1ca657
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-28 14:48:21 +0100

    SDS-5: Configurer Swagger/OpenAPI (classe de configuration)

---
+++ src/main/java/com/smartlogi/sdms/config/AppConfig.java
@@ -0,0 +1,5 @@
+package com.smartlogi.sdms.config;
+import org.springframework.context.annotation.Configuration;
+@Configuration
+public class AppConfig {
+}
---
+++ src/main/java/com/smartlogi/sdms/config/SwaggerConfig.java
@@ -0,0 +1,24 @@
+package com.smartlogi.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI smartLogiOpenAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Smart Delivery Management System API")
+                        .description("API pour gérer et suivre les livraisons de colis de SmartLogi.")
+                        .version("1.0.0")
+                        .license(new License()
+                                .name("Apache 2.0")
+                                .url("http://springdoc.org")));
+    }
+}
