892c470c4431e4e73ac9f49e7a9227a12c944f87
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-30 16:44:57 +0100

    SDMS15:4Créer l'entité d'association ColisProduit (pour le ManyToMan

--- src/main/java/com/smartlogi/sdms/entity/ColisProduit.java
+++ src/main/java/com/smartlogi/sdms/entity/ColisProduit.java
@@ -1,4 +1,41 @@
 package com.smartlogi.sdms.entity;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
-public class ColisProduit {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "colis_produit")
+@Getter
+@Setter
+@NoArgsConstructor
+public class ColisProduit implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "colis_id", nullable = false)
+    private Colis colis;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "produit_id", nullable = false)
+    private Produit produit;
+    @Column(name = "quantite")
+    private Integer quantite;
+    @Column(name = "date_ajout")
+    private LocalDateTime dataAjout;
+
+    @PrePersist
+    protected void onCreate() {
+        dataAjout = LocalDateTime.now();
+    }
 }
