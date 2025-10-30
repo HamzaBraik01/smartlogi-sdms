fbbbd6caa485c14cbb04e2744b82002fc47ff400
HamzaBraik01 <hamzabraik02@gmail.com>
2025-10-30 16:49:06 +0100

    SDS-16:5Créer l'entité HistoriqueLivraison

--- src/main/java/com/smartlogi/sdms/entity/HistoriqueLivraison.java
+++ src/main/java/com/smartlogi/sdms/entity/HistoriqueLivraison.java
@@ -1,4 +1,48 @@
 package com.smartlogi.sdms.entity;
+import com.smartlogi.sdms.entity.enumeration.StatutColis;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.JdbcTypeCode;
+import org.hibernate.type.SqlTypes;
 
-public class HistoriqueLivraison {
+import java.io.Serializable;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "historique_livraison")
+@Getter
+@Setter
+@NoArgsConstructor
+public class HistoriqueLivraison implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    @Column(name = "id", updatable = false, nullable = false)
+    @JdbcTypeCode(SqlTypes.UUID)
+    private String id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "colis_id", nullable = false)
+    private Colis colis;
+
+
+    @Enumerated(EnumType.STRING)
+    @Column(name = "statut")
+    private StatutColis statut;
+
+    @Column(name = "date_changement")
+    private LocalDateTime dateChangement;
+
+    @Column(name = "commentaire")
+    private String commentaire;
+
+
+    @PrePersist
+    protected void onCreate() {
+        if (dateChangement == null) {
+            dateChangement = LocalDateTime.now();
+        }
+    }
 }
